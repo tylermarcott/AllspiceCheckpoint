719bcb48dbd1b28ebaea4171e6ced68dd0657acd
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-11 15:52:16 -0600

    trying to fix account bug

--- AllspiceCheckpoint.client/src/env.js
+++ AllspiceCheckpoint.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-r2l4kl7ifqatct23.us.auth0.com'
 export const clientId = '3ugFmbTisZI8MvM7ux0qRbSLZS8X8Zo1'
