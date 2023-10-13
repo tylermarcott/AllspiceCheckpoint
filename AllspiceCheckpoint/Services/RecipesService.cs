70a5c7334900f7c7a1d087719ef3fef66142de2e
Tyler Marcott <tylerjmarcott@gmail.com>
2023-10-13 12:24:16 -0600

    delete bad request fix

--- AllspiceCheckpoint/Services/RecipesService.cs
+++ AllspiceCheckpoint/Services/RecipesService.cs
@@ -28,15 +28,15 @@ public class RecipesService
 
     internal Recipe GetRecipeById(int recipeId)
     {
-        Recipe recipe = _repo.GetRecipeById(recipeId);
-        return recipe;
+        Recipe foundRecipe = _repo.GetRecipeById(recipeId);
+        if (foundRecipe == null) throw new Exception("Invalid recipe id");
+        return foundRecipe;
     }
 
     internal void DeleteRecipe(int recipeId, string userId)
     {
         // FIXME: have the following error: Code other than 400 returned, make sure you are throwing a bad request when trying to get data that doesn't exist!: expected 204 to be one of [ 400 ]
         Recipe foundRecipe = _repo.GetRecipeById(recipeId);
-        if (foundRecipe == null) throw new Exception("Invalid recipe id");
         if (foundRecipe.CreatorId != userId) throw new Exception("Unauthorized to remove");
         int rows = _repo.DeleteRecipe(recipeId);
         if (rows > 1) throw new Exception("Something bad has happened");
